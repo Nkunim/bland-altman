7659d4edb15d6361f4725d6e14a02fea15d9be86
dmolony3 <dmolony@emory.edu>
2018-04-09 13:08:45 -0400

    Updated args for bland altman

--- bland_altman.py
+++ bland_altman.py
@@ -10,17 +10,25 @@ import matplotlib.pyplot as plt
 import argparse
 from numpy import polyfit
 
-example_text = 'Example Usage: python bland_altman.py --fname=C:\Users\David\Desktop\Plaque_burden_data.csv --var1="Computed Plaque Burden" --var2="Measured Plaque Burden"'
+example_text = r'Example Usage: python bland_altman.py --fname=C:\Users\David\Desktop\Plaque_burden_data.csv \
+--minlimit=0 --maxlimit=100 --var1="Computed Plaque Burden" --var2="Measured Plaque Burden"'
+
 parser = argparse.ArgumentParser(description=\
 'Create Bland-Altman and Concordance correlation coefficient plots', \
 epilog=example_text, formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument('--fname', help='Enter filename')
+parser.add_argument('--minlimit', type=int, help='Enter min limits')
+parser.add_argument('--maxlimit', type=int, help='Enter max limits')
 parser.add_argument('--var1', help='name of variable1')
 parser.add_argument('--var2', help='name of variable2')
+
 
 #optional arguments for x and y labels
 args = parser.parse_args()
 data = pd.read_csv(args.fname)
+
+xlim = [args.minlimit, args.maxlimit]
+ylim = [args.minlimit, args.maxlimit]
 
 # drop an rows that contain nans
 data = data.dropna(axis=0, how='any')
@@ -41,18 +49,18 @@ LoA_neg = mean_diff - 1.96*std_diff
 fig = plt.figure()
 ax1 = fig.add_subplot(121)
 ax1.scatter(mean, diff, s=4)
-ax1.set_xlim((0, 100))
+ax1.set_xlim((xlim[0], xlim[1]))
 xval = ax1.get_xlim()
 ax1.plot(xval, (mean_diff, mean_diff))
 ax1.plot(xval, (LoA_plus, LoA_plus), 'r--')
 ax1.plot(xval, (LoA_neg, LoA_neg), 'r--')
 # set text
-ax1.text(xval[1] - 12.5, LoA_plus + 1.5, '+1.96SD')
-ax1.text(xval[1] - 12.5, LoA_neg + 1.5, '-1.96SD')
-ax1.text(xval[1] - 12.5, mean_diff + 1.5, 'MEAN')
-ax1.text(xval[1] - 12.5, LoA_plus - 3.5, round(LoA_plus, 2))
-ax1.text(xval[1] - 12.5, LoA_neg - 3.5, round(LoA_neg, 2))
-ax1.text(xval[1] - 12.5, mean_diff - 3.5, round(mean_diff, 2))
+ax1.text(xval[1] - (xval[1]/8), LoA_plus + xval[1]*0.015, '+1.96SD')
+ax1.text(xval[1] - (xval[1]/8), LoA_neg + xval[1]*0.015, '-1.96SD')
+ax1.text(xval[1] - (xval[1]/8), mean_diff + xval[1]*0.015, 'MEAN')
+ax1.text(xval[1] - (xval[1]/8), LoA_plus - xval[1]*0.035, round(LoA_plus, 2))
+ax1.text(xval[1] - (xval[1]/8), LoA_neg - xval[1]*0.035, round(LoA_neg, 2))
+ax1.text(xval[1] - (xval[1]/8), mean_diff - xval[1]*0.035, round(mean_diff, 2))
 if args.var1:
     var1 = args.var1
     var2 = args.var2
@@ -66,15 +74,16 @@ ccc = 2*covar.iloc[0, 1]/(covar.iloc[0, 0] + covar.iloc[1, 1] + \
 
 # determine fit
 coeff = polyfit(data.iloc[:, 0], data.iloc[:, 1], 1)
+print('Coefficients are {}'.format(coeff))
 
 ax2 = fig.add_subplot(122)
 ax2.scatter(data.iloc[:, 0], data.iloc[:, 1], s=4)
-ax2.plot((0,100), (coeff[0]*0 + coeff[1], coeff[0]*100 + coeff[1]))
+ax2.plot((xlim[0],xlim[1]), (coeff[0]*0 + coeff[1], coeff[0]*ylim[1] + coeff[1]))
 # plot concordance line
-ax2.plot((0,100), (0, 100), 'k--')
-ax2.text(10, 90, 'CCC={}'.format(round(ccc, 2)))
-ax2.set_xlim((0, 100))
-ax2.set_ylim((0, 100))
+ax2.plot((xlim[0],xlim[1]), (ylim[0], ylim[1]), 'k--')
+ax2.text(xlim[0]+(xlim[1]/20), xlim[1]-(xlim[1]/10), 'CCC={}'.format(round(ccc, 2)))
+ax2.set_xlim((xlim[0], xlim[1]))
+ax2.set_ylim((ylim[0], ylim[1]))
 if args.var1:
     var1 = args.var1
     var2 = args.var2
--- readme.md
+++ readme.md
@@ -2,18 +2,22 @@
 This code generates Bland-altman and concordance correlation coefficient for 2 input variables
 
 ## Input
-The two variables should be saved as a csv file with two columns
+The two variables should be saved as a csv file with two columns (no header)
 
 ## Usage
-From the command window run the file with the path to the csv file provided
+From the command window run the file by providing the path to the csv file and the axis limits
 ```python
-patient_name = python bland_altman.py --fname=path\to\csv\file.csv
+patient_name = python bland_altman.py --fname=path\to\csv\file.csv --minlimit=0 --maxlimit=100
 ```
 Optionally the variables data labels can be specified with the arguments var1 and var2
 ```
-python bland_altman.py --fname=path\to\csv\file.csv --var1="Computed Plaque Burden" --var2="Measured Plaque Burden"
+python bland_altman.py --fname=path\to\csv\file.csv --minlimit=0 --maxlimit=100 --var1="Computed Plaque Burden" --var2="Measured Plaque Burden"
 ```
 
 ## Output
 The code returns the bland-altman plot and the concordance correlatoin plot and coefficient
 ![Alt Text](example_plot.png)
+
+
+## Requirements
+The code requires pandas, numpy and matplotlib
